9151c99688e9564a0ddf7928bd54fbafaa37e1ee
Shrey719 <>
2025-07-07 20:31:01 -0400

    things

--- src/html.js
+++ src/html.js
@@ -1,9 +1,3 @@
-/*
-	markers work like 
-	_m_   -> A regular dom elm or node
-	_mfn -> A function to be attached 
-
-*/
 self.html = function (strings, ...values) {
 	const template = document.createElement("template");
 	const markers = [];
@@ -11,18 +5,15 @@ self.html = function (strings, ...values) {
 	const htmlString = strings
 		.map((str, i) => {
 			const val = values[i];
-
-			if (typeof val === "function" && /<\s*$/.test(str) || val instanceof Node) {
-				const node = val();
+	
+			if (val instanceof Node || val instanceof DocumentFragment || Array.isArray(val)) {
 				const marker = `<!--_m_:${markers.length}-->`;
-				markers.push(node);
+				markers.push(val);
 				return str + marker;
 			}
-
 			if (val && typeof val === "object" && "subscribe" in val) {
-				const container = document.createElement("span"); 
+				const container = document.createElement("span");
 				val.subscribe((v) => {
-					container.textContent = ""; 
 					if (v instanceof Node) {
 						container.replaceChildren(v);
 					} else {
@@ -47,18 +38,21 @@ self.html = function (strings, ...values) {
 	template.innerHTML = htmlString;
 	const root = template.content;
 
-	// replace the normal ones
 	const walker = document.createTreeWalker(root, NodeFilter.SHOW_COMMENT);
 	while (walker.nextNode()) {
 		const comment = walker.currentNode;
 		const match = comment.nodeValue.match(/^_m_:(\d+)$/);
 		if (match) {
 			const node = markers[parseInt(match[1])];
-			comment.replaceWith(node);
+			if (Array.isArray(node)) {
+				comment.replaceWith(...node);
+			} else {
+				comment.replaceWith(node);
+			}
+
 		}
 	}
 
-	// replace the onclicks
 	root.querySelectorAll("*").forEach((el) => {
 		for (const attr of [...el.attributes]) {
 			const match = attr.name.match(/^on:([a-z]+)$/);
